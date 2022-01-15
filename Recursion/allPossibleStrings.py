878ecd6b17ea6f7f948f07e88f99d14824a47b3a
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-01-15 13:32:04 -0800

    Update allPossibleStrings.py

--- Recursion/allPossibleStrings.py
+++ Recursion/allPossibleStrings.py
@@ -1,1 +1,20 @@
+def reverse(st):
+    l = []
+    print(mainR(st,len(st),l,prefix = ""))
 
+def mainR(st,n,l,prefix):
+    if n < 1:
+        return 
+    if n == 1:
+        for j in st:
+            l.append(prefix+j)
+
+    for i,val in enumerate(st):
+        
+        mainR(st,n-1,l,prefix+val)
+        
+    return l
+        
+        
+st = "abc"
+reverse(st)
