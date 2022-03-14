097beb518d1f4bfa0116862b8ed11811886f1112
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-03-14 13:39:48 -0700

    Update prob241.py

--- LeetCode/prob241.py
+++ LeetCode/prob241.py
@@ -10,27 +10,43 @@ class Solution(object):
             
         
         def compute(s, exp, dicti):
-            if len(s) == 1:
-                return s
+            if "+" not in s and "-" not in s and "*" not in s:
+                # print(s)
+                # print("hello")
+                return [s]
             if s in dicti:
                 return dicti[s]
             results = []
             for i in range(len(s)):
                 if s[i] in exp:
+                    # print(len(s[:i]),len(s[i+1:]))
                     right = compute(s[:i], exp, dicti)
+                    # print(right)
                     left = compute(s[i+1:],exp, dicti)
+                    # print(left)
                     # print(right, left)
                     for j in right:
                         for k in left:
                             results.append(check(j, k, s[i]))
+                            # print(results)
             dicti[s] = results
-            return results
+            return dicti[s]
         
         dicti = {}
         exp = ["-", "+", "*"]
         k = compute(s, exp, dicti)
-        return k
+        # print(k)
+        if len(k) == 0:
+            list1 = []
+            list1.append(int(s))
+            return list1
+        else:
+            return k
 
             
 
                     
+                
+                        
+                        
+                
