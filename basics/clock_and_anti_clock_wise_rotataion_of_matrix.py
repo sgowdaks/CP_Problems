3a90d132200e28e34ad3422e8e3148ae904b686d
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-08-30 11:23:03 -0700

    Create clock_and_anti_clock_wise_rotataion_of_matrix.py

---
+++ basics/clock_and_anti_clock_wise_rotataion_of_matrix.py
@@ -0,0 +1,37 @@
+mat = [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12],
+     [13, 14, 15, 16]]
+     
+#transpose
+for i in range(0, len(mat)):
+    for j in range(i, len(mat)):
+        mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+        
+        
+#cloack-wise
+
+for i in range(0, len(mat)):
+    left = 0
+    right = len(mat) - 1
+    while left < right:
+        mat[i][left], mat[i][right] = mat[i][right], mat[i][left]
+        left = left + 1
+        right = right - 1
+        
+#anticlock-wise
+
+for i in range(0, len(mat)):
+    top = 0
+    down = len(mat) - 1 
+    while top < down:
+        mat[top][i], mat[down][i] = mat[i][top], mat[i][down]
+        top = top + 1
+        down = down - 1
+
+for i in range(len(mat)):
+    print(mat[i])
+        
+        
+    
+        
