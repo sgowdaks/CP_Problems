df0fe652e948e9448f4a78bd224ee463632a02be
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-08-31 21:38:54 -0700

    Update count_good_nodes_in_binary_tree.py

--- datastructure/trees/count_good_nodes_in_binary_tree.py
+++ datastructure/trees/count_good_nodes_in_binary_tree.py
@@ -1,32 +1,27 @@
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
+def goodNodes(self, root: TreeNode) -> int:
         def recurse(root, max_):
             if root == None:
                 return 0
             else:
                 res = 0
-                count = 0
                 if root.left != None:
                     if root.left.val >= max_:
-                        count = count + 1
-                        res = count + recurse(root.left, root.left.val)
+						#will change the max_ value if greater than previous in left tree
+                        res = 1 + recurse(root.left, root.left.val)
                     else:
-                        res = count + recurse(root.left, max_)
+                        res = recurse(root.left, max_)
                         
-                count = 0
                 if root.right != None:
                     if root.right.val >= max_:
-                        count = count + 1
-                        res += count + recurse(root.right, root.right.val )
+						#will change the max_ value if greater than previous in right tree
+                        res += 1 + recurse(root.right, root.right.val )
                     else:
-                        res += count + recurse(root.right, max_)
+                        res += recurse(root.right, max_)
                 return res
                     
                             
         
-        count = recurse(root, max_ = root.val)
-        if root.val != None:
-            return count + 1
-        else:
+        if root == None:
             return 0
-        
+        else:
+            return (recurse(root, max_ = root.val) + 1)
