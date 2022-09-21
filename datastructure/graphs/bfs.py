4c1a0a80fd564dea2437ace3ae1c828e4c0bb5f3
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-09-21 13:44:32 -0700

    Update bfs.py

--- datastructure/graphs/bfs.py
+++ datastructure/graphs/bfs.py
@@ -1,3 +1,4 @@
+#method 1
 def bfsOfGraph(self, V, adj):
         list1 = []
         stack = []
@@ -15,3 +16,25 @@ def bfsOfGraph(self, V, adj):
                     newl[i] = True
             
         return list1
+
+
+#method2
+class Solution:    
+    #Function to return Breadth First Traversal of given graph.
+    def bfsOfGraph(self, V, adj):
+        # code here
+        answer = []
+        stack = []
+        stack.append(0)
+        visited = [False] * V
+        while len(stack) > 0:
+            if visited[stack[0]] == False:
+                k = stack.pop(0)
+                visited[k] = True
+                for i in adj[k]:
+                    stack.append(i)
+                answer.append(k)
+            else:
+                stack.pop(0)
+        
+        return answer
