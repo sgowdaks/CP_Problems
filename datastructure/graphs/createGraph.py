e136eec95b37d49619f3f451b2e066f3bd086967
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-09-21 12:27:04 -0700

    Update createGraph.py

--- datastructure/graphs/createGraph.py
+++ datastructure/graphs/createGraph.py
@@ -1,3 +1,4 @@
+#method 1
 class Node:
     def __init__(self, data):
         self.data = data
@@ -35,3 +36,44 @@ graph.addEdge(1,2)
 graph.addEdge(1,3)
 graph.addEdge(3,2)
 graph.disAdjecent()
+
+
+#method 2
+class Graph:
+    def __init__(self):
+        self.dicti = {}
+        
+    #directed graph
+    def directed_graph(self, start, end):
+        if start not in self.dicti:
+            self.dicti[start] = []
+        if end not in self.dicti:
+            self.dicti[end] = []
+        
+        self.dicti[start].append(end)
+        
+    #undirected graph
+    def undirected_graph(self,start, end):
+        if start not in self.dicti:
+            self.dicti[start] = []
+        if end not in self.dicti:
+            self.dicti[end] = []
+        
+        self.dicti[start].append(end)
+        self.dicti[end].append(start)
+        
+    def print_graph(self):
+        for i in self.dicti.keys():
+            for j in self.dicti[i]:
+                print(i, "->", j)
+ 
+graph = Graph()       
+edges = input()
+for i in range(int(edges)):
+    node1, node2 = input().split()
+    graph.directed_graph(node1, node2)
+
+graph.print_graph()
+    
+
+
