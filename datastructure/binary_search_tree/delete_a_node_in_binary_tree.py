95b828682069b102023fbbfe7cf416debc5da2e7
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-09-27 16:09:08 -0700

    Create delete_a_node_in_binary_tree.py

---
+++ datastructure/binary_search_tree/delete_a_node_in_binary_tree.py
@@ -0,0 +1,29 @@
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+            if root:
+                if root.val > key:
+                    root.left = self.deleteNode(root.left,key)
+
+                elif root.val < key:
+
+                    root.right = self.deleteNode(root.right,key)
+                else:
+                    if root.left == None and root.right == None:
+                        return None
+                    if root.left == None or root.right == None:
+                        if root.left:
+                            return root.left
+                        else:
+                            return root.right
+
+                    else:
+                        temp = root.left
+                        while temp.right !=None:
+                            temp=temp.right
+                        root.val=temp.val
+
+                        root.left = self.deleteNode(root.left,temp.val)
+
+            return root
+                
+            
