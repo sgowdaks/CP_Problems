af7ee0ca4a50f28f0347527b0f7f30849156589d
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-10-11 17:39:52 -0700

    Update jump_game_1.py

--- LeetCode/jump_game_1.py
+++ LeetCode/jump_game_1.py
@@ -1,22 +1,34 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
+        goals = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] + i >= goals:
+                print(nums[i], nums[i]+i, goals)
+                goals = i
+        
+        if goals == 0:
             return True
-        if nums[0] == 0:
+        else:
             return False
+
         
-        l = 0
-        r = 0
+#         if len(nums) == 1:
+#             return True
+#         if nums[0] == 0:
+#             return False
         
-        while r < len(nums) - 1:
-            new_ = 0
-            # print(l, r)
-            for i in range(l,r+1):
-                new_ = max(new_, i + nums[i])
-            if new_ == r:
-                return False
-            l = r + 1
-            r = new_
-            # print(l, r)
-            if r >= len(nums) - 1:
-                return True
+#         l = 0
+#         r = 0
+        
+#         while r < len(nums) - 1:
+#             new_ = 0
+#             # print(l, r)
+#             for i in range(l,r+1):
+#                 new_ = max(new_, i + nums[i])
+#             if new_ == r:
+#                 return False
+#             l = r + 1
+#             r = new_
+#             # print(l, r)
+#             if r >= len(nums) - 1:
+#                 return True
