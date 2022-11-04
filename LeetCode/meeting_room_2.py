5d571dc3035cde7901843dd781b2146da0e31b36
Shivani Gowda KS <56445524+sgowdaks@users.noreply.github.com>
2022-11-03 22:04:23 -0700

    Create meeting_room_2.py

---
+++ LeetCode/meeting_room_2.py
@@ -0,0 +1,24 @@
+class Solution:    
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if len(intervals) == 1:
+            return 1
+        intervals = sorted(intervals, key=lambda x:x[0])
+        count = 1
+        interval = []
+        
+        count = 1
+        
+        print(intervals)
+        
+        heap = [intervals[0][1]]
+        
+        for i in range(1, len(intervals)):
+            earlist_end = heap[0]
+            
+            if intervals[i][0] >= earlist_end:
+                heapq.heapreplace(heap, intervals[i][1])
+            else:
+                heapq.heappush(heap, intervals[i][1])
+        
+        print(heap)
+        return len(heap)
